9f8a32e8cb3b4484f35eb4cccbd23624a84a0a48
Sumanthbadisa <2300030061@kluniversity.in>
2025-09-22 21:23:48 +0530

    second commit

--- CICD BACKEND/CICDPROJECTBackend/src/main/java/com/klu/CICDPROJECT/CicdprojectApplication.java
+++ CICD BACKEND/CICDPROJECTBackend/src/main/java/com/klu/CICDPROJECT/CicdprojectApplication.java
@@ -2,12 +2,13 @@ package com.klu.CICDPROJECT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdprojectApplication {
+public class CicdprojectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicdprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CicdprojectApplication.class, args);
+    }
 
 }
